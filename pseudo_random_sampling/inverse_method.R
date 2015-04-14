62603e5230fabc0257a83ac10e64bb76ccb5169a
hjerpe <hjerpe.adam@gmail.com>
2015-04-14 20:31:09 +0200

    Improving names

--- pseudo_random_sampling/inverse_method.R
+++ pseudo_random_sampling/inverse_method.R
@@ -1,24 +1,26 @@
 # Illustration of pseudo random generated number using the inverse method
 
-inverse_method <- function(n_samples, F_inverse) {
-    # Returns n_samples of a random variable having the distribution function
+sample_using_inverse_method <- function(n_samples, F_inverse) {
+    # Returns n_samples from a random variable having the distribution function
     # F.
 
     # ARGS:
-    # n_samples - Number of drawn samples
-    # F_inverse - Inverse function of the desired sampling distribution F
+    # n_samples - Number of drawn samples.
+    # F_inverse - Inverse function of the distribution function governing the
+    # drawn samples.
 
     uniform_samples <- runif(n=n_samples, min=0, max=1)
     return (F_inverse(uniform_samples))
 }
 
 
-inverse_exponential <- function(x) -log(1 - x)
+inverse_exponential_d <- function(x) -log(1 - x)
 
 # Illustrate drawn samples having exp(1) distributions using the inverse
 # method
 N_SAMPLES = 10000
-exponential_samples <- inverse_method(N_SAMPLES, inverse_exponential)
+exponential_samples <- sample_using_inverse_method(N_SAMPLES,
+                                                   inverse_exponential_d)
 x_range <- seq(0, 20, length=300)
 
 plot_title <- c('10000 uniformly drawn samples mapped by the',
