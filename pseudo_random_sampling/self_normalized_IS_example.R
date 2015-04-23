3595c89e9f9eaf634284e9ca3bcc9c2bf093b5f6
hjerpe <hjerpe.adam@gmail.com>
2015-04-23 18:59:51 +0200

    Improved readability

--- pseudo_random_sampling/self_normalized_IS_example.R
+++ pseudo_random_sampling/self_normalized_IS_example.R
@@ -7,32 +7,39 @@ source('importance_sampling.R')
 # Where,
 # phi(x) = x^2
 # w(x) = z(x) / g(x) = cf(x) / g(x).
+# Further the deniminator E_g [w(x)] is an estimator for the normalizing
+# constant c.
 
-instrumental_sample_distribution <- function(n_samples) {
+instrumental_sample_function <- function(n_samples) {
     # Return n_samples samples from U(-p/2, pi/2)
     runif(n_samples, min=-pi/2, max=pi/2)
 }
 
 importance_weight_function <- function(x) { (pi) * exp(cos(x)**2) }
 
-phi <- function(x) { x**2 }
-
-sampling_numerator <- function(n_samples) {
-    importance_sampling(n_samples, phi, instrumental_sample_distribution,
-                        importance_weight_function)
+expected_value_numerator <- function(used_n_samples) {
+    importance_sampling(
+        used_n_samples,
+        phi=function(x) { x**2 },
+        instrumental_sample_function=instrumental_sample_function,
+        importance_weight_function=importance_weight_function
+        )
     }
 
-sampling_denominator <- function(n_samples) {
-    importance_sampling(n_samples, phi=function(x) { 1 },
-                        instrumental_sample_distribution,
-                        importance_weight_function)
+sampling_denominator <- function(used_n_samples) {
+    importance_sampling(
+        used_n_samples,
+        phi=function(x) { 1 },
+        instrumental_sample_function=instrumental_sample_function,
+        importance_weight_function=importance_weight_function
+        )
     }
 
 N_SAMPLES <- 800
 NORMALIZING_CONSTANT <- 5.50843 # True normalizing constant given by WolphramAlpha
 VARIANCE <- 0.587201 # True variance given by WolphramAlpha
 x_range <- seq(from=1, N_SAMPLES)
-samples_numerator <- mapply(x_range, FUN=sampling_numerator)
+samples_numerator <- mapply(x_range, FUN=expected_value_numerator)
 samples_denominator <- mapply(x_range, FUN=sampling_denominator)
 
 vector_variances <- samples_numerator / samples_denominator
