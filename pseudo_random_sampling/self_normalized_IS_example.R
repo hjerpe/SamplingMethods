7e2b157869809662370b222f4962d1d5c52c23e8
hjerpe <hjerpe.adam@gmail.com>
2015-04-23 19:13:57 +0200

    Improved namings for readability

--- pseudo_random_sampling/self_normalized_IS_example.R
+++ pseudo_random_sampling/self_normalized_IS_example.R
@@ -2,7 +2,7 @@ source('importance_sampling.R')
 # Example computing the variance of the r.v. X having the distribution
 # f = exp(cost^2(x)) / c using self-normalized importance sampling.
 
-# The variance calculated by E_f [phi(X)] = E_g [phi(X) * w(x)] / E_g [w(x)]
+# The variance is calculated by E_f [phi(X)] = E_g [phi(X) * w(x)] / E_g [w(x)]
 # for some instrumental distribution g s.t. g = 0 implies that f = 0.
 # Where,
 # phi(x) = x^2
@@ -17,18 +17,18 @@ instrumental_sample_function <- function(n_samples) {
 
 importance_weight_function <- function(x) { (pi) * exp(cos(x)**2) }
 
-expected_value_numerator <- function(used_n_samples) {
+expected_value_numerator <- function(summed_n_samples) {
     importance_sampling(
-        used_n_samples,
+        summed_n_samples,
         phi=function(x) { x**2 },
         instrumental_sample_function=instrumental_sample_function,
         importance_weight_function=importance_weight_function
         )
     }
 
-sampling_denominator <- function(used_n_samples) {
+sampling_denominator <- function(summed_n_samples) {
     importance_sampling(
-        used_n_samples,
+        summed_n_samples,
         phi=function(x) { 1 },
         instrumental_sample_function=instrumental_sample_function,
         importance_weight_function=importance_weight_function
@@ -36,15 +36,25 @@ sampling_denominator <- function(used_n_samples) {
     }
 
 N_SAMPLES <- 800
-NORMALIZING_CONSTANT <- 5.50843 # True normalizing constant given by WolphramAlpha
-VARIANCE <- 0.587201 # True variance given by WolphramAlpha
-x_range <- seq(from=1, N_SAMPLES)
-samples_numerator <- mapply(x_range, FUN=expected_value_numerator)
-samples_denominator <- mapply(x_range, FUN=sampling_denominator)
+# True normalizing constant calculated by WolphramAlpha
+NORMALIZING_CONSTANT <- 5.50843
+ # True variance calculated by WolphramAlpha
+VARIANCE <- 0.587201
+vec_n_samples <- seq(from=1, N_SAMPLES)
 
-vector_variances <- samples_numerator / samples_denominator
+expected_values_numerator <- mapply(vec_n_samples, FUN=expected_value_numerator)
+expected_values_denominator <- mapply(vec_n_samples, FUN=sampling_denominator)
 
-plot(x_range, vector_variances, type = 'l')
+vector_estimated_variance <- expected_values_numerator / expected_values_denominator
+
+# Plot the estimates obtained using different number of samples.
+x_label <- c('N summed samples')
+plot(vec_n_samples, vector_estimated_variance, type = 'l',
+     xlab=x_label,
+     ylab=c('Estimated variance'))
 abline(a=VARIANCE, b=0, lty=2, lwd=2, col='red')
-plot(x_range, samples_denominator, type = 'l')
+plot(vec_n_samples, expected_values_denominator, type = 'l',
+     xlab=x_label,
+     ylab=c('c'),
+     main=c('Estimated normalizing constant c for exp(cos^2(x)) / c'))
 abline(a=NORMALIZING_CONSTANT, b=0, lty=2, lwd=2, col='red')
