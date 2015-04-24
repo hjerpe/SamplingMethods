a721c81f414c288e68f0d3e8de1c9b8e15647dd6
hjerpe <hjerpe.adam@gmail.com>
2015-04-24 16:54:31 +0200

    Improved docs for importance_sampling.R

--- pseudo_random_sampling/importance_sampling.R
+++ pseudo_random_sampling/importance_sampling.R
@@ -1,13 +1,12 @@
 # Generate random numbers with importance sampling
 
-importance_sampling <- function(n_samples, phi, instrumental_sample_function,
-                                importance_weight_function) {
-    # Returns the expected value tau = E_f [phi(X)], the expected value of
-    # phi(X) under the density function f. The transformed calculation is
-    # E_g [phi(x) * w(x)].
+importance_sampling <- function(n_samples, phi, importance_weight_function,
+                                instrumental_sample_function) {
+    # Returns an estimate of the expected value tau = E_g [w(x) * phi(X)]
+    # that is the expected value of (phi(X) * w(X)) under the density g.
     # The density function g may only be zero where f = 0.
 
-    # Algorithm: Estimate tau = E_f [phi(X)] = E_g [phi(X) * w(X)]
+    # Algorithm: Estimate tau = E_g [phi(X) * w(X)]
     # for i = [1:n]
     #   draw Xi ~ g
     # end
@@ -19,10 +18,19 @@ importance_sampling <- function(n_samples, phi, instrumental_sample_function,
     
     # ARGS:
     # n_samples - Number of samples used in the estimate tau_n.
-    # phi - Fucntion to calculate expected value of under f.
+    # phi - Function that represents the first term in the calculated
+    # expected value E_g [phi(X) * w(X)].
+    # importance_weight_function - Second term used in E_g [phi(X) * w(X)].
     # instrumental_sample_function - Function returning samples having
     # distribution g outlined in the Algorithm part.
-    # importance_weight_function - weight function w outlined in the Algorithm p.
+    # RETURNS: The expected value E_g [phi(X) * w(X)].
+
+    # COMMENTS: Valid if g = 0 implies f = 0
+    # If w is chosen as f/g (x) then E_f [phi(X)] = E_g[phi(X) * w(X)]
+    # If w is chosen as (f*c)/g (x) where c is the normalizing constant for
+    # the distribution f.
+    # Then E_f [phi(X)] = E_g [phi(X) * w(X)] / E_g [w(X)], and
+    # E_g [w(X)] is an estimator of c.
     draws <- instrumental_sample_function(n_samples)
     return (sum(phi(draws) * importance_weight_function(draws))/n_samples)
 }
